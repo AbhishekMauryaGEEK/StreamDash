63caca4e2c7043eae0ebc16a7e5bd4727fc1f46b
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-12-22 12:51:10 +0530

    'change'

---
+++ full_stack/src/models/user.model.js
@@ -0,0 +1,46 @@
+import moongoose, { Schema } from "mongoose";
+const userschema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String, //clouedinary url
+        required: true,
+    },
+    coverImage: {
+        type: String, //cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type:String,
+        required:[true,'password is required']
+    },
+    refreshToken:{
+        type:String
+    }
+}, { timestamps: true }
+)
+export const User = moongoose.model('USerschema', userschema)
