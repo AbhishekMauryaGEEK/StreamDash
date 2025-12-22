eea09b3a481e2b6ecbb2c023feba48220cfa07e8
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-12-23 02:40:43 +0530

    'change'

--- full_stack/src/models/user.model.js
+++ full_stack/src/models/user.model.js
@@ -1,5 +1,7 @@
 import moongoose, { Schema } from "mongoose";
-const userschema = new Schema({
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+const userSchema = new Schema({
     username: {
         type: String,
         required: true,
@@ -35,12 +37,20 @@ const userschema = new Schema({
         }
     ],
     password: {
-        type:String,
-        required:[true,'password is required']
+        type: String,
+        required: [true, 'password is required']
     },
-    refreshToken:{
-        type:String
+    refreshToken: {
+        type: String
     }
 }, { timestamps: true }
 )
-export const User = moongoose.model('USerschema', userschema)
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+userSchema.method.isPasswordCorrect = async function (password) {
+  return await   bcrypt.compare(password,this.password)
+}
+export const User = moongoose.model('USerschema', userSchema)
