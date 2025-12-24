fb97fc19da6bc76c9a39ab86a742bc97eef3b7ea
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-12-25 00:00:54 +0530

    'change'

--- full_stack/src/utiles/clouedinary.js
+++ full_stack/src/utiles/clouedinary.js
@@ -5,3 +5,25 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const upload = async (locafilepath)=>{
+    try{
+        if(!locafilepath)
+            return null
+        const transfer = await cloudinary.uploader.upload(locafilepath,{
+            resource_type:"auto"
+        })
+        console.log("file is uploaded on cloudinary",transfer.url);
+        return transfer.url
+    }
+    catch(error){
+        fs.unlinkSync(locafilepath)
+        return null;
+    }
+}
+
+cloudinary.v2.uploader.upload("https://upload.wikimedia. org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+{ public_id: "olympic_flag" },
+function (error, result) { console.log(result); });
+
+export {upload}
