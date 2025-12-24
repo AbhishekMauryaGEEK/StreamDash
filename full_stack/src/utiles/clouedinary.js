bccc31f2ebadbd4fca0773137c0bf2529c708761
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-12-25 00:35:03 +0530

    'change'

--- full_stack/src/utiles/clouedinary.js
+++ full_stack/src/utiles/clouedinary.js
@@ -21,7 +21,6 @@ const upload = async (locafilepath)=>{
         return null;
     }
 }
-
 cloudinary.v2.uploader.upload("https://upload.wikimedia. org/wikipedia/commons/a/ae/Olympic_flag.jpg",
 { public_id: "olympic_flag" },
 function (error, result) { console.log(result); });
