db170080c1f7526c4586868ba1a1a89bc29f41e5
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2026-01-08 21:44:03 +0530

    'change'

--- full_stack/src/controllers/user.controller.js
+++ full_stack/src/controllers/user.controller.js
@@ -1,8 +1,21 @@
 import { asynchandler } from "../utiles/asynchandler.js";
 
-const registerUser =asynchandler(async(req,res)=>{
-     res.status(200).json({
-        message:"okjijg"
-    })
+const registerUser = asynchandler(async (req, res) => {
+    
+    //get user details from frontend
+    //validation -not empty
+    //check if user  already exist :email,username
+    //check for image ,check for avatar
+    //upload them to cloudinary,avatar
+    //create user object -create entry in db
+    // remove password and refresh token from response 
+    //check for user creation 
+    //return res 
+    const { username, email, fullname, password } = req.body
+console.log(`username:${username}`)
+console.log(`password:${password}`)
+console.log(`email:${email}`)
+console.log(`fullname:${fullname}`)
 })
-export {registerUser}
+
+export { registerUser }
--- full_stack/src/routes/user.routes.js
+++ full_stack/src/routes/user.routes.js
@@ -1,4 +1,5 @@
 import {Router} from "express"
+import {upload} from "../middlewares/multer.middleware.js"
 import { registerUser } from "../controllers/user.controller.js"
 const router=Router()
 router.route("/register").post(registerUser)
