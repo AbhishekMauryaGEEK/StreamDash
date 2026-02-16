7c0a218aa61d601d1736723e55f0c43334d5e0db
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2026-02-16 22:47:19 +0530

    'change'

--- full_stack/src/routes/Subscriber.route.js
+++ full_stack/src/routes/Subscriber.route.js
@@ -2,4 +2,4 @@ import { Router } from "express";
 import { toggleSubscription } from "../controllers/user.Subscribtion.js";
 import { verifyJWT } from "../middlewares/AUTH.middleware.js";
 const router =Router()
-router.route("/")
+router.route("/subscribercheck").patch(verifyJWT,toggleSubscription);
