9b454cd198c3c67541e8c9fb380f2067c273d795
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2026-03-26 02:49:07 +0530

    'change'

--- backend/Readme.md
+++
@@ -1,1 +0,0 @@
-# ths is a full stack project 
--- backend/src/routes/healthcheck.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { healthcheck } from "../controllers/healthcheck.controller.js";
-
-const HealthcheckRouter = Router();
-
-HealthcheckRouter.route("/").get(healthcheck);
-
-export default HealthcheckRouter;
