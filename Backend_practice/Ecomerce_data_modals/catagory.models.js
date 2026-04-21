b4a37b69e7c8f9a429d5e3bd816b5110f6f61749
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2026-04-22 02:34:51 +0530

    directry switch

--- Backend_practice/Ecomerce_data_modals/catagory.models.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const Catagory =new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-    },
-},{timestamps:true})
-export const  catagory=mongoose.model('Category',Catagory)
